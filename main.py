e14f14610ad41316858d4458765b01ab74daae6c
Amos Mesika <92472409+AmosMes@users.noreply.github.com>
2022-05-03 21:32:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ operations = {
 }
 should_continue = True
 while  should_continue:
+# Printing the keys from the dictionary so the user will be able to pick a mathematical operation.
     num1 = int(input(f"What is the first number?: "))
     num2 = int(input(f"What is the second number?: "))
 
@@ -42,6 +43,7 @@ while  should_continue:
       print(key)
     operation_key = input(f"Pick an operation key from the line above: ")
 
+# Calculation function based on the dictionary and the functions defined above, instead of the 7 lines of if statements below.
     calculation_function = operations[operation_key]
     answer = calculation_function(num1, num2)
 # if operation_key == "+":
@@ -55,6 +57,7 @@ while  should_continue:
   
     print(f"{num1} {operation_key} {num2} = {answer}")
 
+# The condition for the while loop , if yes the user will be able to do another calculation, if no the app will close.    
     calc_again = input(f"Would you like to calc again?: yes or no ")
     if calc_again == "no":
         should_continue = False
